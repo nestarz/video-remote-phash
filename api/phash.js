4713aa59b59f26ab7f64e3e2647738484aac4168
nestarz <elias.rhouzlane@balthuslab.com>
2022-05-10 23:57:15 +0200

    fix: try tflite

--- api/phash.js
+++ api/phash.js
@@ -1,28 +1,56 @@
+import { Interpreter } from "node-tflite";
+import fs from "fs";
+import path from "path";
+import { createCanvas, loadImage } from "canvas";
 import axios from "axios";
-import { PrepareLobe, isLambda } from "tfjs-node-lambda-helpers";
 
-console.log(process.versions);
+const modelPath = path.resolve(
+  __dirname,
+  "..",
+  "static",
+  "mobilenet_v1_1.0_224_quant.tflite"
+);
 
-const baseUrl = isLambda()
-  ? `https://${process.env.VERCEL_URL}`
-  : `http://localhost:3000`;
-const prepareLobe = PrepareLobe(`${baseUrl}/static/model`);
+function createInterpreter() {
+  const modelData = fs.readFileSync(modelPath);
+  return new Interpreter(modelData);
+}
 
-let model;
+async function getImageInput(arrayBuffer, size) {
+  const canvas = createCanvas(size, size);
+  const context = canvas.getContext("2d");
+  const image = await loadImage(arrayBuffer);
+  context.drawImage(image, 0, 0, size, size);
+  const data = context.getImageData(0, 0, size, size);
+
+  const inputData = new Uint8Array(size * size * 3);
+
+  for (let i = 0; i < size * size; ++i) {
+    inputData[i * 3] = data.data[i * 4];
+    inputData[i * 3 + 1] = data.data[i * 4 + 1];
+    inputData[i * 3 + 2] = data.data[i * 4 + 2];
+  }
+
+  return inputData;
+}
 
 export default async (req, res) => {
-  const lobe = await prepareLobe.next();
-  if (!lobe.done) {
-    return res.status(lobe.value.statusCode).json(lobe.value);
-  } else {
-    model || (model = lobe.value);
-  }
-  const imageUrl = encodeURI(decodeURIComponent(req.query.url));
+  const interpreter = createInterpreter();
+  interpreter.allocateTensors();
 
-  const response = await axios.get(imageUrl, {
+  const imageUrl = encodeURI(decodeURIComponent(req.query.url));
+  const { data } = await axios.get(imageUrl, {
     responseType: "arraybuffer",
   });
-  const results = model.predict(response.data);
+  const inputData = await getImageInput(data, 224);
+  interpreter.inputs[0].copyFrom(inputData);
 
-  return res.status(200).json({ results: results.Confidences });
+  interpreter.invoke();
+
+  const outputData = new Uint8Array(1001);
+  interpreter.outputs[0].copyTo(outputData);
+
+  const maxIndex = outputData.indexOf(Math.max(...Array.from(outputData)));
+
+  res.end(JSON.stringify(maxIndex));
 };
