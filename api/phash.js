571525c28773c04c5ee0faa685c234b9e5821b95
nestarz <elias.rhouzlane@balthuslab.com>
2022-05-10 11:03:29 +0200

    fix: phash use square

--- api/phash.js
+++ api/phash.js
@@ -32,28 +32,38 @@ export default catchHandle(async (req, res) => {
   if (!raw) throw Error("Missing video url");
   const url = encodeURI(decodeURIComponent(raw));
 
-  const crop = await exec(
+  const { duration, crop } = await exec(
     ffmpegPath,
     `-i ${url} -t 1 -vf cropdetect -f null pipe:1`.split(" "),
-    (res) => (d) =>
-      apply(/.*crop=(.*?)($|\n| ).*/.exec(d), (v) => (v ? res(v[1]) : null))
+    (res) => {
+      const data = {};
+      return (d) => {
+        data.crop = /.*crop=(.*?)($|\n| ).*/.exec(d)?.[1] ?? data.crop;
+        data.duration =
+          apply(/.*Duration: (.*?),.*/.exec(d)?.[1], (v) =>
+            v ? +v?.split(":")?.reduce((acc, time) => 60 * acc + +time) : v
+          ) ?? data.duration;
+        if (data.crop && data.duration) res(data);
+      };
+    }
   );
+
+  const isVideo = duration > 0;
 
   const buffer = await new Promise((res) => {
     const stream = PassThrough();
     const buffers = [];
     stream.on("data", (buf) => buffers.push(buf));
     stream.on("end", () => res(Buffer.concat(buffers)));
-    const isVideo = !!crop;
-    console.log(crop);
+    const N = Math.round(Math.sqrt(Math.floor(duration)));
     ffmpeg(url)
       .outputOptions([
         "-frames 1",
         `-vf ${[
-          isVideo && !crop.includes("-") && `crop=${crop}`,
+          crop && !crop.includes("-") && `crop=${crop}`,
           "crop=min(ih\\,iw):min(ih\\,iw),scale=144:144",
           isVideo &&
-            "select='(gte(t,2))*(isnan(prev_selected_t)+gte(t-prev_selected_t,2))',tile=5x5",
+            `select='(gte(t,1))*(isnan(prev_selected_t)+gte(t-prev_selected_t,1))',tile=${N}x${N}`,
         ]
           .filter((v) => v)
           .join(",")}`,
