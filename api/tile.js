f976441e6b556ad6c5557faced19a36b7977d169
Elias Rhouzlane <elias.rhouzlane@gmail.com>
2022-05-11 05:19:18 +0200

    fix: add tile api

---
+++ api/tile.js
@@ -0,0 +1,77 @@
+import ffmpeg from "fluent-ffmpeg";
+import { path as ffmpegPath } from "@ffmpeg-installer/ffmpeg";
+import { PassThrough } from "stream";
+import { spawn } from "child_process";
+
+ffmpeg.setFfmpegPath(ffmpegPath);
+
+const apply = (v, fn) => fn(v);
+const exec = async (cmd, args, onData) => {
+  const proc = spawn(cmd, args);
+  return new Promise((res, rej) => {
+    const data = {};
+    const ok = () => res(data);
+    proc.stdout.on("data", ok);
+    proc.stderr.setEncoding("utf8");
+    proc.stderr.on("data", onData(data, ok, rej));
+    proc.on("close", ok);
+  });
+};
+
+export default async (req, res) => {
+  const { url: raw } = req.query;
+  if (!raw) throw Error("Missing video url");
+  const url = encodeURI(decodeURIComponent(raw));
+
+  const { duration, crop } = await exec(
+    ffmpegPath,
+    [
+      `-i ${url}`,
+      "-t 3",
+      "-vf",
+      "select='isnan(prev_selected_t)+gte(t-prev_selected_t,1)',format=gbrp,tblend=all_mode=difference,curves=all='0/0 0.3/1 1/1',cropdetect",
+      `-vsync vfr -f null pipe:1`,
+    ].flatMap((d) => (d[0] === "-" ? d.split(" ") : d)),
+    (data, res) => (s) => {
+      data.crop = /.*crop=(.*?)($|\n| ).*/.exec(s)?.[1] ?? data.crop;
+      data.duration =
+        apply(/.*Duration:[\n\r\s]+(.*?),.*/.exec(s)?.[1], (v) =>
+          v ? +v?.split(":")?.reduce((acc, time) => 60 * acc + +time) : v
+        ) ?? data.duration;
+      if (data.crop && data.duration) res();
+    }
+  );
+
+  const buffer = await new Promise((res) => {
+    const stream = PassThrough();
+    const buffers = [];
+    stream.on("data", (buf) => buffers.push(buf));
+    stream.on("end", () => res(Buffer.concat(buffers)));
+    const isVideo = duration > 0;
+    const N = Math.round(Math.sqrt(Math.floor(duration)));
+    ffmpeg(url)
+      .outputOptions([
+        `-vf ${[
+          crop && !crop.includes("-") && `crop=${crop}`,
+          "crop=min(ih\\,iw):min(ih\\,iw),scale=144:144",
+          isVideo &&
+            `select='(isnan(prev_selected_t)+gte(t-prev_selected_t,1))',tile=${N}x${N}`,
+          `scale=1024:1024`,
+        ]
+          .filter((v) => v)
+          .join(",")}`,
+        "-frames:v 1",
+        "-vcodec png",
+        "-f rawvideo",
+      ])
+      .on("error", console.error)
+      .pipe(stream, { end: true });
+  });
+
+  res
+    .writeHead(200, {
+      "Content-Type": "image/png",
+      "Cache-Control": `s-maxage=${86400 * 30}, stale-while-revalidate`,
+    })
+    .end(buffer);
+};
