0c2884ef547e1dc0a861514f16ffffbbf9df8558
nestarz <elias.rhouzlane@balthuslab.com>
2022-05-12 22:56:15 +0200

    fix: revert to ss perc

--- api/tile.js
+++ api/tile.js
@@ -1,7 +1,9 @@
+
+   
 import ffmpeg from "fluent-ffmpeg";
 import { path as ffmpegPath } from "@ffmpeg-installer/ffmpeg";
-import { spawn } from "child_process";
 import { PassThrough } from "stream";
+import { spawn } from "child_process";
 
 ffmpeg.setFfmpegPath(ffmpegPath);
 
@@ -18,22 +20,12 @@ const exec = async (cmd, args, onData) => {
   });
 };
 
-const transpose = (m) => m[0].map((_, i) => m.map((x) => x[i]));
-const range = (N) => [...Array(N).keys()];
-const outer = ([v1, v2]) => v1.map((x) => v2.map((y) => `${x}_${y}`));
-const tile = (N, fn) =>
-  range(N).map((i, _, arr) => arr.slice(0, i).map(fn).join("+") || 0);
-const layout = (N) =>
-  outer([tile(N, (k) => `w${k * N}`), tile(N, (k) => `h${k}`)])
-    .flat()
-    .join("|");
-
 export default async (req, res) => {
   const { url: raw } = req.query;
   if (!raw) throw Error("Missing video url");
   const url = encodeURI(decodeURIComponent(raw));
 
-  console.time("crop");
+  console.time("ffmpeg");
   const { duration, crop } = await exec(
     ffmpegPath,
     [
@@ -44,7 +36,7 @@ export default async (req, res) => {
       `-vsync vfr -f null pipe:1`,
     ].flatMap((d) => (d[0] === "-" ? d.split(" ") : d)),
     (data, res) => (s) => {
-      data.crop = /.*(crop=.*?)($|\n| ).*/.exec(s)?.[1] ?? data.crop;
+      data.crop = /.*crop=(.*?)($|\n| ).*/.exec(s)?.[1] ?? data.crop;
       data.duration =
         apply(/.*Duration:[\n\r\s]+(.*?),.*/.exec(s)?.[1], (v) =>
           v ? +v?.split(":")?.reduce((acc, time) => 60 * acc + +time) : v
@@ -52,44 +44,35 @@ export default async (req, res) => {
       if (data.crop && data.duration) res();
     }
   );
-  console.timeEnd("crop");
 
-  console.time("ffmpeg");
-  const k0 = 10;
-  const I = duration / k0;
-  const N = Math.round(Math.sqrt(k0));
-  const K = N * N;
-  const p = [crop, "crop=min(ih\\,iw):min(ih\\,iw)", "scale=144:144"].join(",");
-
-  const buffer = await new Promise((res, rej) => {
+  const buffer = await new Promise((res) => {
     const stream = PassThrough();
     const buffers = [];
     stream.on("data", (buf) => buffers.push(buf));
     stream.on("end", () => res(Buffer.concat(buffers)));
-
-    ffmpeg()
+    const isVideo = duration > 0;
+    const K = 50;
+    const I = duration / K;
+    const N = Math.round(Math.sqrt(K));
+    ffmpeg(url)
       .outputOptions([
-        ...range(K).flatMap((k) => [
-          `-ss ${k * I}`,
-          "-noaccurate_seek",
-          `-i ${url}`,
-        ]),
+        `-vf ${[
+          crop && !crop.includes("-") && `crop=${crop}`,
+          "crop=min(ih\\,iw):min(ih\\,iw),scale=144:144",
+          isVideo &&
+            `select='(gte(t\,${I}))*(isnan(prev_selected_t)+gte(t-prev_selected_t\,${I}))',tile=${N}x${N}`,
+          `scale=1024:1024`,
+        ]
+          .filter((v) => v)
+          .join(",")}`,
         "-frames:v 1",
-        `-filter_complex ${range(K)
-          .map((k) => `[${k}:v]${p}[v${k}]`)
-          .join(";")};${range(K)
-          .map((k) => `[v${k}]`)
-          .join("")}xstack=inputs=${K}:layout=${layout(N)},scale=1024:1024`,
         "-vcodec png",
         "-f rawvideo",
       ])
-      .on("start", console.log)
-      .on("error", rej)
+      .on("error", console.error)
       .pipe(stream, { end: true });
   });
   console.timeEnd("ffmpeg");
-
-  console.log(Buffer.byteLength(buffer));
 
   res
     .writeHead(200, {
