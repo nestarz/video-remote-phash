f98b3d9b8fc55720aface365880081b2ad6e2697
nestarz <elias.rhouzlane@balthuslab.com>
2022-05-16 23:20:09 +0200

    fix: use last layer

--- api/helpers/mobileNet.js
+++ api/helpers/mobileNet.js
@@ -1,12 +1,11 @@
 import "@tensorflow/tfjs-backend-cpu";
 import * as tflite from "tfjs-tflite-node";
 import * as tf from "@tensorflow/tfjs-core";
-import { bufferToTensor } from "./utils.js";
-import { resolve } from "path";
+import { bufferToTensor, download } from "./utils.js";
 
-const modelPath = resolve(
-  "static",
-  "lite-model_imagenet_mobilenet_v3_large_100_224_feature_vector_5_default_custom100.tflite"
+const modelPath = await download(
+  "https://storage.googleapis.com/tfhub-lite-models/google/lite-model/imagenet/mobilenet_v3_large_100_224/feature_vector/5/default/1.tflite",
+  "lite-model_imagenet_mobilenet_v3_large_100_224_feature_vector_5_default_4.tflite"
 );
 const [H, W, C] = [224, 224, 3];
 const tfliteModel = await tflite.loadTFLiteModel(modelPath);
